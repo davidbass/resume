d6e74ca9d130b3ed3358ecb601e421b13476aae8
David Bass <david@davidbass.com>
2019-02-08 22:57:36 -0800

    now highlights background

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -11,13 +11,19 @@ export class Highlight extends Component {
   }
 
   getHighlightedText(text, searchFor) {
+    // thanks to https://stackoverflow.com/a/43235785 
     // Split text on searchFor term, include term itself into parts, ignore case
     // var parts = text.split(new RegExp(`(${searchFor})`, 'gi'));
+    let thisClassName = '';
     if (searchFor) {
+      if (text.includes(searchFor)) {
+        thisClassName = 'highlightbg';
+        console.log('found "' + searchFor + '" in "' + text + '"');
+      }
       // Split on searchFor term and include term into parts, ignore case
       const parts = text.split(' ');    // convert the string into an array
       return <span> { parts.map((part, i) => 
-          <span key={i} style={part.toLowerCase() === searchFor.toLowerCase() ? { fontWeight: 'bold' } : { } }>
+          <span key={i} className={ thisClassName } style={part.toLowerCase() === searchFor.toLowerCase() ? { background: 'orange' } : { } }>
             { part + ' ' }
           </span>)
       } </span>;
